4a6709b294d0c533a014f44814cc78299a78fedc
ScarletRevenger <josefclare@outlook.com>
2026-03-24 12:43:40 +0000

    completed compare integers exercise

---
+++ compareIntergers.py
@@ -0,0 +1,11 @@
+x = int(input("Enter an integer: "))
+y = int(input("Enter another integer: "))
+
+if x > y:
+    print(f"{x} is greater than {y}.")
+
+if x < y:
+    print(f"{x} is less than {y}.")
+
+if x == y:
+    print(f"{x} is equal to {y}")
